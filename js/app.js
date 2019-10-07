02d34ef1fd7c340cc5d9a55534383880ad1da094
Bernard Barnes III <bjbarnesiii@gmail.com>
2019-10-07 14:04:58 -0400

    Added all project files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    alert('Hello, Thanks for viewing my tribute page!');
+});
+// coded by @ChaituVR
+const projectName = 'tribute-page';
+localStorage.setItem('example_project', 'Tribute Page');
+
